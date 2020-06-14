f56bbaa9b05868b3514352db552081e7ff7ce9cd
yeetusfetus46 <59057975+yeetusfetus46@users.noreply.github.com>
2020-06-14 19:39:27 -0400

    Initial commit

---
+++ ytdl/templates/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $this->get('title', 'Youtube Downloader'); ?></title>
+	<meta name="keywords" content="Video downloader, download youtube, video download, youtube video, youtube downloader, download youtube FLV, download youtube MP4, download youtube 3GP, php video downloader"/>
+	<meta name="description" content="Video downloader, download youtube, video download, youtube video, youtube downloader, download youtube FLV, download youtube MP4, download youtube 3GP, php video downloader"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<link href="../css/primary.css" rel="stylesheet">
+		<link href="./css/custom.css" rel="stylesheet">
+</head>
+<body>
+      <nav class="black-text navbar-expand-lg navbar-dark fixed-top" id="navbar">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+          <div class="collapse navbar-collapse" id="navbarContent">
+          <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link waves-effect waves-light" style="color:#000000;" href="./contributors.html">Staff & Contributors</a></li>
+            <li class="nav-item"><a class="nav-link waves-effect waves-light" style="color:#000000;" href="https://github.com/titaniumnetwork-dev">Github</a></li>
+            </ul>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a class="nav-link waves-effect waves-light" style="color:#000000;" href="./">Home</a></li>
+            <li class="nav-item"><a class="nav-link waves-effect waves-light" style="color:#000000;" href="./selection3.html">Games</a></li>
+            <li class="nav-item"><a class="nav-link waves-effect waves-light" style="color:#000000;" href="./selection2.html">Movies/TV</a></li>
+            <li class="nav-item"><a class="nav-link waves-effect waves-light" style="color:#000000;" href="./youtube.html">Youtube</a></li>
+          </div>
+        </div>  
+      </nav>
+<div class="container">
